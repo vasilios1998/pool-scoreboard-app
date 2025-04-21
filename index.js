52c59bc222c932069d9c86a8be3570b0205448f2
vasilios1998 <goulasvasilios@gmail.com>
2025-04-21 18:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -180,3 +180,15 @@ const App = () => {
 };
 
 export default App;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
